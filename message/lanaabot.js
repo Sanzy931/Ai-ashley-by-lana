dafe17680c3c2349739ee2b7dc8949783c939ee5
Sanzy931 <diamondf866@gmail.com>
2024-09-01 14:34:48 +0700

    uploud via termux

---
+++ message/lanaabot.js
@@ -0,0 +1,122 @@
+
+
+export default async function(prefix,command) {
+global.mess = {
+wait: 'Tunggu sebentar ya kak',
+query: 'Masukan query',
+search: 'Searching...',
+scrap: '*Scrapping...*',
+success: 'Berhasil!',
+limit: `[❕] Limit kamu sudah habis silahkan beli limit kamu di owner , ketik .owner`,
+claimOn: `Kamu sudah melakukan claim sebelumnya, Harap claim lagi pada jam `,
+wrongFormat: 'Format salah, coba liat lagi di menu',
+
+error: {
+stick: 'bukan sticker itu:v',
+api: 'Error api atau linkya mungkin',
+Iv: 'Linknya error:v',
+link : "Link error!"
+},
+
+block:{
+Bowner: `Maaf kak command tersebut telah di block oleh owner`,
+Bsystem: `Command tersebut telah di block oleh system karena terjadi error`
+},
+
+ only: {
+prem : 'Premium special features! Chat owner to get Premium access!',
+group: 'Fitur ini dapat digunakan di Dalam Group!',
+ownerB: 'Fitur Khusus Owner Bot!',
+owner: 'Fitur Khusus Owner Bot!',
+admin: 'Fitur dapat Digunakan oleh Admin Group!',
+Badmin: 'Fitur dapat Digunakan Setelah Bot menjadi ADMIN!'
+ }
+  
+ }
+}
+
+
+
+global.fileStackApi ="Amje6cHl6QWm1tHLLQbX7z" //daftar di filestack
+global.apiflash ="9b9e84dfc18746d4a19d3afe109e9ea4"
+
+global.botcahx ='lanagalau'
+global.lana = 'lanagalau' // api.botcahx.live
+global.calip = 'caliphkey' //
+
+
+
+global.gcounti = {
+'prem' : 60, //limit prem
+'user' : 20 //limit
+} 
+
+/**
+  * Created by Maulana / Lana
+  * Base : Dittaz & Irfan Haryanto
+  * Contact me on WhatsApp wa.me/6281775445373
+  * Subscribe me on Youtube : https://youtube.com/@maulanabot
+  
+  * If you want to buy an updated script that is not encrypted, please WhatsApp me
+*/
+
+
+
+// Maulana Bot & Lana hanya 1 Orang
+// SCRIPT BY : LANA DEV & Dittaz
+// EROR?? CHAT GROUP : https://chat.whatsapp.com/JgJHpvM7K6UAYVlT2zQPFA
+// JANGAN DI HAPUS Y
+// JUAL SCRIPT?? NERAKA PALING BAWAH
+global.lanabotz = 'https://chat.whatsapp.com/LFbp0YPFkdNJ9kIx43VK1t'
+global.lannn ='GROUP WHATSAPP'
+global.lananih ='Simple botz V.3'
+global.lananich ='LanaX Dev Botz'
+global.nan = '╔════════'
+global.non = '╠══'
+global.nin = '╠'
+global.nah = '╚════════'
+global.yan = '.'
+global.app = '*❏*'
+global.apz = '*▸*'
+global.maulana = 'https://telegra.ph/file/f22b0622f1e895db51aa9.jpg'
+
+
+
+
+global.newsletterJid = "120363285522801458@newsletter"
+///
+global.newsletterName = "LANA BOTZ"
+//===================//
+
+global.fotoRandom = [
+  "https://telegra.ph/file/59a2583b604f3cb255cb4.jpg",
+  "https://telegra.ph/file/9ef8c957f050f3d98cda9.jpg",
+  "https://telegra.ph/file/c2eb090748fad09d7bd4f.jpg",
+  "https://telegra.ph/file/63e8a49b86824c3402c2a.jpg",
+  "https://telegra.ph/file/0aa9d587a19e37a0b0122.jpg",
+  "https://telegra.ph/file/ce7d3cb43e2e8f6208b7d.jpg",
+  "https://telegra.ph/file/1932cf21673a42e2f8482.jpg",
+  "https://telegra.ph/file/06bb7b62cd3e6f4fe6fb7.jpg",
+  "https://telegra.ph/file/5f4b5eca3cf5ecbda9002.jpg",
+  "https://telegra.ph/file/e4de3b5cac270a458f4cc.jpg",
+];
+
+
+//━━━━━━━[ GAK USAH DI UBAH LEE ]━━━━━━━━//
+
+global.replyType = "web"
+global.setwelcome = "type2"
+global.autoblockcmd = false
+global.autoReport = true
+global.autoLevel = true
+global.autoSticker = false // true untuk kirim foto otomatis jd stiker
+
+global.docType = "docx" // ga ush di ganti
+global.Qoted = "ftoko" // ga ush di ganti
+global.autoBio = true // tanya lana kalo mau aktifin auto bio
+
+
+global.fake = botName
+global.Console = false
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
---
+++ settings.js
@@ -0,0 +1,61 @@
+
+/**
+  * Created by Maulana / Lana
+  * Base : Dittaz & Irfan Haryanto
+  * Contact me on WhatsApp wa.me/6281775445373
+  * Subscribe me on Youtube : https://youtube.com/@maulanabot
+  
+  * If you want to buy an updated script that is not encrypted, please WhatsApp me
+*/
+
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const version = require("baileys/package.json").version 
+global.require = require
+global.language = "id"
+global.nomerOwner =" "// ganti sesuka hati lo
+global.nomerOwner2 =""// ganti sesuka hati lo
+global.runWith = " "
+global.ownerName = "Lana Botz"// ganti sesuka hati lo
+global.botName = "Lanabotz" // ganti sesuka hati lo
+global.sessionName ="session" //untuk session rm- rf session
+global.baileysMd = true
+global.Intervalmsg = 1000 //detik
+global.antiSpam = false
+
+global.copyright = `lanDev`
+global.baileysVersion = `baileys ${version}`
+global.fake1 ="LanaBotz"
+global.packName = "Lanabotz"
+global.authorName = "LanTeam"
+
+global.gamewaktu = 60
+global.limitCount = 20 // untuk limit user
+
+global.qris = "https://telegra.ph/file/116a9e29f1101403719e9.jpg"
+
+/** 
+ NOTE : 
+ 
+ * APIKEY BELI SENDIRI BANG *
+ * GAK ADA APIKEY?? FITUR BOTZ GAK BERJALAN *
+ 
+*/
+
+/*==========《 API Key 》============*/
+
+// TEMPAT NARO API FITUR  LOLHUMAN
+global.apikey =" " 
+
+
+/*==========《 API Key 》============*/
+
+
+/**
+  * Created by Maulana / Lana
+  * Base : Dittaz & Irfan Haryanto
+  * Contact me on WhatsApp wa.me/6281775445373
+  * Subscribe me on Youtube : https://youtube.com/@maulanabot
+  
+  * If you want to buy an updated script that is not encrypted, please WhatsApp me
+*/
