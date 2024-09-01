a83f9b50e05fb77550b959aafc66c848e00003a2
Sanzy931 <129380156+Sanzy931@users.noreply.github.com>
2024-09-01 15:18:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,21 +13,21 @@ const require = createRequire(import.meta.url)
 const version = require("baileys/package.json").version 
 global.require = require
 global.language = "id"
-global.nomerOwner =" "// ganti sesuka hati lo
-global.nomerOwner2 =""// ganti sesuka hati lo
+global.nomerOwner ="6283121675788"// ganti sesuka hati lo
+global.nomerOwner2 ="628174715364"// ganti sesuka hati lo
 global.runWith = " "
-global.ownerName = "Lana Botz"// ganti sesuka hati lo
-global.botName = "Lanabotz" // ganti sesuka hati lo
+global.ownerName = "Ash"// ganti sesuka hati lo
+global.botName = "Ashley ai" // ganti sesuka hati lo
 global.sessionName ="session" //untuk session rm- rf session
 global.baileysMd = true
 global.Intervalmsg = 1000 //detik
 global.antiSpam = false
 
-global.copyright = `lanDev`
+global.copyright = `ash`
 global.baileysVersion = `baileys ${version}`
-global.fake1 ="LanaBotz"
-global.packName = "Lanabotz"
-global.authorName = "LanTeam"
+global.fake1 ="Ashley"
+global.packName = "Ashley"
+global.authorName = "Sanzyteam"
 
 global.gamewaktu = 60
 global.limitCount = 20 // untuk limit user
@@ -45,17 +45,7 @@ global.qris = "https://telegra.ph/file/116a9e29f1101403719e9.jpg"
 /*==========《 API Key 》============*/
 
 // TEMPAT NARO API FITUR  LOLHUMAN
-global.apikey =" " 
+global.apikey ="edbe8aace2459ff838de3f75" 
 
 
 /*==========《 API Key 》============*/
-
-
-/**
-  * Created by Maulana / Lana
-  * Base : Dittaz & Irfan Haryanto
-  * Contact me on WhatsApp wa.me/6281775445373
-  * Subscribe me on Youtube : https://youtube.com/@maulanabot
-  
-  * If you want to buy an updated script that is not encrypted, please WhatsApp me
-*/
